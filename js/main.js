b6f0106f9f3b4a1995f3db7761f111e9562f4c79
Manvi Goel <manvi16244@iiitd.ac.in>
2018-06-13 02:25:05 +0530

    Add local storage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 // listener for form submit
-document.getElementById("myForm").addEventListener("submit", saveBookmark);
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
-function saveBookmark(){
-	console.log("It works");
+//save bookmark
+function saveBookmark(e){
+	
+	var siteName = document.getElementById('siteName').value;
+	var siteURL = document.getElementById('siteURL').value;
+	
+
+	var bookmark = {
+		name : siteName,
+		url : siteURL
+	}
+	
+
+	//local storage test
+	localStorage.setItem('test', 'manvi');
+
+	//prevent form from resubmitting
+	e.preventDefault();
 }
